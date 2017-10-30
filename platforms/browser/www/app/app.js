50ed8e3507418e6753ec57f419f47f71f104c6b5
vijaypaliwal <vijay.paliwal@hotmail.com>
2017-10-30 13:34:54 +0530

    update
    update

--- platforms/browser/www/app/app.js
+++ platforms/browser/www/app/app.js
@@ -20,57 +20,57 @@ app.config(function ($routeProvider) {
 
     $routeProvider.when("/orders", {
         controller: "ordersController",
-        templateUrl: "/app/views/orders.html"
+        templateUrl: "app/views/orders.html"
     });
 
     $routeProvider.when("/refresh", {
         controller: "refreshController",
-        templateUrl: "/app/views/refresh.html"
+        templateUrl: "app/views/refresh.html"
     });
 
     $routeProvider.when("/tokens", {
         controller: "tokensManagerController",
-        templateUrl: "/app/views/tokens.html"
+        templateUrl: "app/views/tokens.html"
     });
 
     $routeProvider.when("/associate", {
         controller: "associateController",
-        templateUrl: "/app/views/associate.html"
+        templateUrl: "app/views/associate.html"
     });
     
 
     $routeProvider.when("/SelectBillFields", {
         controller: "SelectBillFieldsController",
-        templateUrl: "/app/views/SelectBillFields.html"
+        templateUrl: "app/views/SelectBillFields.html"
     });
 
 
     $routeProvider.when("/SelectBillFieldsNew", {
         controller: "SelectBillFieldsNewController",
-        templateUrl: "/app/views/SelectBillFieldsNew.html"
+        templateUrl: "app/views/SelectBillFieldsNew.html"
     });
 
     $routeProvider.when("/InvoiceDetails", {
         controller: "InvoiceDetailsController",
-        templateUrl: "/app/views/InvoiceDetails.html"
+        templateUrl: "app/views/InvoiceDetails.html"
     });
 
 
     $routeProvider.when("/CompanyDetails", {
         controller: "CompanyDetailsController",
-        templateUrl: "/app/views/CompanyDetails.html"
+        templateUrl: "app/views/CompanyDetails.html"
     });
 
 
     $routeProvider.when("/invoice", {
         controller: "invoiceController",
-        templateUrl: "/app/views/invoice.html"
+        templateUrl: "app/views/invoice.html"
     });
 
 
     $routeProvider.when("/SelectFields", {
         controller: "SelectFieldsController",
-        templateUrl: "/app/views/SelectFields.html"
+        templateUrl: "app/views/SelectFields.html"
     });
 
     $routeProvider.otherwise({ redirectTo: "/home" });

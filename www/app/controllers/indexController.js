0b8f9774c68aa77f98d6d62949d7b3e8760dadc1
dashrath123 <dashrath.k@shivamitconsultancy.com>
2017-11-10 17:05:31 +0530

    Scan barcode
    Scan barcode

--- www/app/controllers/indexController.js
+++ www/app/controllers/indexController.js
@@ -15,5 +15,20 @@ app.controller('indexController', ['$scope', '$location', 'authService', functio
     console.log($scope.authentication)
 
     
+    $scope.ScanData = function (ID) {
+        var scanner = cordova.plugins.barcodeScanner;
+
+        scanner.scan(function (result) {
+
+            $(ID).val(result.text);
+            $(ID).trigger("input");
+
+
+        }, function (error) {
+          alert("Scanning failed: ", error);
+        });
+    }
+
+
 
 }]);
